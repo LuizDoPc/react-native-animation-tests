0a84ca1720e629d51159e9dde43b6689761ad6a5
luizdopc <luizotaviosoares@outlook.com>
2019-08-13 15:20:31 -0300

    pulse hook animation

--- App.js
+++ App.js
@@ -1,109 +1,68 @@
-/* eslint-disable comma-dangle */
 /* eslint-disable quotes */
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState } from "react";
-import { ScrollView, View, Text, Image, Animated } from "react-native";
+/* eslint-disable comma-dangle */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef } from "react";
+import { View, Animated, Text } from "react-native";
 
-const HEADER_MAX_HEIGHT = 120;
-const HEADER_MIN_HEIGHT = 70;
-const PROFILE_IMAGE_MAX_HEIGHT = 80;
-const PROFILE_IMAGE_MIN_HEIGHT = 40;
+const Box = ({ backgroundColor = "#3cae6f", scale = 1 }) => (
+  <Animated.View
+    style={[
+      {
+        width: 100,
+        height: 100,
+        backgroundColor,
+        transform: [{ scale }]
+      }
+    ]}
+  />
+);
 
-const App = () => {
-  const [scrollY] = useState(new Animated.Value(0));
+const usePulse = (startDelay = 500) => {
+  const scale = useRef(new Animated.Value(1)).current;
 
-  const headerHeight = scrollY.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-    extrapolate: "clamp"
-  });
-  const profileImageHeight = scrollY.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
-    extrapolate: "clamp"
-  });
-  const profileImageMarginTop = scrollY.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    outputRange: [
-      HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
-      HEADER_MAX_HEIGHT + 5
-    ],
-    extrapolate: "clamp"
-  });
-  const headerZindex = scrollY.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    outputRange: [0, 1],
-    extrapolate: "clamp"
-  });
-  const headerTitleBottom = scrollY.interpolate({
-    inputRange: [
-      0,
-      HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
-      HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT,
-      HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT + 26
-    ],
-    outputRange: [-20, -20, -20, 0],
-    extrapolate: "clamp"
-  });
+  const pulse = () => {
+    Animated.sequence([
+      Animated.timing(scale, { toValue: 1.2 }),
+      Animated.timing(scale, { toValue: 0.8 })
+    ]).start(() => pulse());
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => pulse(), startDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return scale;
+};
+
+const App = ({ count }) => {
+  const scale = usePulse();
+  const scale2 = usePulse(750);
+
   return (
-    <Animated.View style={{ flex: 1 }}>
-      <Animated.View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "lightskyblue",
-          height: headerHeight,
-          zIndex: headerZindex,
-          alignItems: "center"
-        }}
-      >
-        <Animated.View
-          style={{ position: "absolute", bottom: headerTitleBottom }}
-        >
-          <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>
-            Luiz Soares
-          </Text>
-        </Animated.View>
-      </Animated.View>
-      <ScrollView
-        style={{ flex: 1 }}
-        scrollEventThrottle={16}
-        onScroll={Animated.event([
-          { nativeEvent: { contentOffset: { y: scrollY } } }
-        ])}
-      >
-        <Animated.View
-          style={{
-            height: profileImageHeight,
-            width: profileImageHeight,
-            borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
-            borderColor: "white",
-            borderWidth: 3,
-            overflow: "hidden",
-            marginTop: profileImageMarginTop,
-            marginLeft: 10
-          }}
-        >
-          <Image
-            source={{
-              uri:
-                "https://s3.amazonaws.com/TWFiles/158490/userAvatar/tf_C8368988-A5BE-508D-8258ABBF94285226.avatar.jpg"
-            }}
-            style={{ flex: 1, width: null, height: null }}
-          />
-        </Animated.View>
-        <View>
-          <Text style={{ fontWeight: "bold", fontSize: 26, paddingLeft: 10 }}>
-            Luiz Soares
-          </Text>
-        </View>
-
-        <View style={{ height: 1000 }} />
-      </ScrollView>
-    </Animated.View>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    >
+      <Box scale={scale2} backgroundColor="#1f9cb8" />
+      <Text>{count}</Text>
+      <Box scale={scale} />
+    </View>
   );
 };
 
-export default App;
+export default class Wrapper extends React.Component {
+  state = { count: 1 };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(state => ({
+        count: state.count + 1
+      }));
+    }, 500);
+  }
+
+  render() {
+    return <App count={this.state.count} />;
+  }
+}
